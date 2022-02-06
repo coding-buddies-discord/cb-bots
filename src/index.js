b5bd11c4ba8b5b780049af0ad6d465569145c0c5
earn <waream2@gmail.com>
2022-02-05 23:13:57 -0500

    feat: add intents to client

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable quotes */
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
-const { token } = require('..config.json');
+const { token } = require('../config.json');
 
 // Create a new client instance
-const client = new Client();
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
