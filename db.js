a8ebda0f5ba9a7700961b6c3e9da32c242266c32
Claudi0-V <claudiov.dejesus@protonmail.com>
2022-02-24 19:19:06 -0300

    adds lastPointsGivenBy

--- db.js
+++ db.js
@@ -38,12 +38,21 @@ export const addUserToPoints = (userId) => {
 	db.write();
 };
 
+
 export const giveUserAPoint =  (userId, interaction) => {
 	const newPoint = new PointsObject(interaction.author.id, Date.now(), interaction.channel.name);
+	const newPointGivenBy = new PointsObject(interaction.author.id, Date.now());
 	points[userId].pointsReceived.push(newPoint);
+	points[userId].lastPointsGivenBy.push(newPointGivenBy);
+
+
 	db.write()
 }
-	
+
+
+
+
+
 export const countGivenPoint = (userId) => {
 // this needs to be an object with where the key becomes the channel and the value is a count.
 // go get keys, check if channel is in key, if so add one to value, if not add it to object and set to zero
