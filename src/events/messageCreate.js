03cd6a0c2fb53f9c90f85fcda64777a60fff679e
Claudi0-V <claudiov.dejesus@protonmail.com>
2022-02-25 18:24:56 -0300

    creates givePoints function

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -3,8 +3,35 @@ import config from "../../config.js";
 import client from "../index.js";
 import * as messageReplies from "../message_replies/index.js";
 import { getUserIdFromMention } from "../utils/getUserIdFromMention.js";
+import { isUserValid } from "../utils/isUserValid.js"
 import { addUserToPoints, giveUserAPoint, countGivenPoint, testDates } from "../../db.js";
 
+
+
+async function givePoint(command, interaction) {
+				const mentionId = getUserIdFromMention(command);
+				if(!mentionId) return //there's no mention id;
+				const validUser = await isUserValid(interaction, mentionId);
+				if(!validUser) {
+					return interaction.channel.send(`Sorry <@!${interaction.author.id}>, It appears that ${command} is an **invalid user** or **isn't currently in the server**`);
+				}
+				if (interaction.author.id === mentionId) {
+					return interaction.channel.send(`Sorry <@!${interaction.author.id}>, You cannot give a point to yourself.`);
+				}
+				// try to add the user to the DB, if they are already there
+				// db function will reject this
+				addUserToPoints(mentionId);
+				const canAddPoint = testDates(mentionId, interaction)
+				if (!canAddPoint){
+					return interaction.channel.send(`Sorry <@!${interaction.author.id}>, You need to wait at least 1min to give point to <@!${mentionId}> again`)
+				}
+				if (canAddPoint) {
+					giveUserAPoint(mentionId, interaction);
+					const {username, discriminator} = interaction.author;
+					const userPoints = countGivenPoint(mentionId, interaction.channel.name);
+					interaction.channel.send(`Point added! Now <@!${mentionId}> has **${userPoints} points**`);
+			}	
+}
 
 export default {
 	name: "messageCreate",
@@ -48,26 +75,10 @@ export default {
 				}
 			}
 			if (command.at(1) === "@" && command.includes(suffix)) { 
-				const mentionId = getUserIdFromMention(command);
-				if(!mentionId) return //there's no mention id;
-				if (interaction.author.id === mentionId) {
-					return interaction.channel.send(`Sorry <@!${interaction.author.id}>, You cannot give a point to yourself.`);
-				}
-				// try to add the user to the DB, if they are already there
-				// db function will reject this
-				addUserToPoints(mentionId);
-				const canAddPoint = testDates(mentionId, interaction)
-				if (!canAddPoint){
-					return interaction.channel.send(`Sorry <@!${interaction.author.id}>, You need to wait at least 1min to give point to <@!${mentionId}> again`)
-				}
-				if (canAddPoint) {
-					giveUserAPoint(mentionId, interaction);
-					const {username, discriminator} = interaction.author;
-					const userPoints = countGivenPoint(mentionId, interaction.channel.name);
-					interaction.channel.send(`Point added! Now <@!${mentionId}> has **${userPoints} points**`);
-			}
+				givePoint(command, interaction)
 			}
 		});
 
 	},
 };
+
