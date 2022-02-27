56821a04c4ef44912a373848a0ca0b4cd832a44a
Emily <emilydta@users.noreply.github.com>
2022-02-27 23:52:01 +1100

    fix: add missing break from !points switch

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -48,6 +48,7 @@ export default {
 					break;
 				case "!points":
 					channelPointsMessage(interaction);
+					break;
 				case "!help":
 					messageReplies.helpCommand(interaction, client);
 					break;
