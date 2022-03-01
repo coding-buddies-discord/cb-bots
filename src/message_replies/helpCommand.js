8e30745fcddb7df5f700841e457f86495b1df0cf
Emily <emilydta@users.noreply.github.com>
2022-03-01 15:57:36 +1100

    refactor: create new commands/desc using objects

--- src/message_replies/helpCommand.js
+++ src/message_replies/helpCommand.js
@@ -1,13 +1,26 @@
 import { MessageEmbed } from "discord.js";
 
 const helpCommand = (interaction, client) => {
+
+    //Add new commands to this array.
+    const commandsArray = [
+        {command: `!ping`, description: `View the bot response time in milliseconds.`},
+        {command: `@user++`, description: `If a user has been helpful to you, mention their username and add ++ to give them a point.`},
+        {command: `!points`, description: `View the top 5 users who have attained the most points in the current channel.`}
+    ]
+
+   function outputCommands() {
+        const newArray = []
+        for (let i = 0; i < commandsArray.length; i++) {
+            newArray.push(`\`${commandsArray[i].command}\`: ${commandsArray[i].description.concat(`\n`)}`);
+        }
+        return newArray.join(" ");
+   }
+
     const embed = new MessageEmbed()
         .setTitle("Bot Buddy Commands List")
         .setColor("RANDOM")
-        .setDescription(`
-            \`!ping\`: View the bot response time in milliseconds.
-\`@user++\`: If a user has been helpful to you, mention their username and add ++ to give them a point.
-\`!points\`: View the top 5 users who have attained the most points in the current channel.`)
+        .setDescription(`${outputCommands()}`)
     interaction.reply({ embeds: [embed] });
 }
 
