9526e2caf2aa3b234c6fc29fd6de831dc86ec262
Claudi0-V <claudiov.dejesus@protonmail.com>
2022-03-03 11:42:56 -0300

    fix: improve the massage due to changes for channelID

--- src/message_replies/channelPoints.js
+++ src/message_replies/channelPoints.js
@@ -2,15 +2,23 @@ import { channelPoints } from "../../db.js";
 import { isUserValid } from "../utils/isUserValid.js";
 
 const channelPointsMessage = async (interaction) => {
-	const channelName = interaction.channel.name;
-	const topPointEarners = channelPoints(channelName, 5);
-	let message = `Those are the top ${topPointEarners.length} on **${channelName}**:\n`;
-	for (const { userID, points } of topPointEarners) {
-		const { username } = await isUserValid(interaction, userID);
-		message += `${username}: ${points} Points\n`;
-	}
+	const  { channelId } = interaction;
+	const topPointEarners = channelPoints(channelId, 5);
 
-	interaction.reply(message);
+  if (topPointEarners.length === 0) return interaction.reply("There's no points in this channel yet.");
+  
+  const validUsers = [];
+  for (const { userID, points } of topPointEarners) { 
+    if (points === 0 ) continue;
+    const {username, validUser} = await isUserValid(interaction, userID);
+    if (validUser ) validUsers.push({username, points});
+  }
+  
+  const message = validUsers.reduce((acc, {username, points}) => {
+    return acc + `${username}: ${points} Points\n`;
+  }, `Those are the top ${validUsers.length} on <#${channelId}>:\n`);
+	
+  interaction.reply(message);
 };
 
 export default channelPointsMessage;
