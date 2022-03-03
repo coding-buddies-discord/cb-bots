239e2ff19df47b21a67a98c1e286a5fdada79984
earn <waream2@gmail.com>
2022-03-02 22:25:10 -0500

    chore: linting the file

--- src/message_replies/helpCommand.js
+++ src/message_replies/helpCommand.js
@@ -1,21 +1,21 @@
 import { MessageEmbed } from "discord.js";
-import commandsList from "./commandsList.js";
+import commandsList from "../../models/commandsList.js";
 
 const helpCommand = (interaction, client) => {
-   function outputCommands() {
-        const newArray = []
-        for (let i = 0; i < commandsList.length; i++) {
-            newArray.push(`\`${commandsList[i].command}\`: ${commandsList[i].description}\n`);
-        }
-        newArray.sort();
-        return newArray.join("");
-   }
+	function outputCommands() {
+		const newArray = [];
+		for (let i = 0; i < commandsList.length; i++) {
+			newArray.push(`\`${commandsList[i].command}\`: ${commandsList[i].description}\n`);
+		}
+		newArray.sort();
+		return newArray.join("");
+	}
 
-    const embed = new MessageEmbed()
-        .setTitle("Bot Buddy Commands List")
-        .setColor("RANDOM")
-        .setDescription(`${outputCommands()}`)
-    interaction.reply({ embeds: [embed] });
-}
+	const embed = new MessageEmbed()
+		.setTitle("Bot Buddy Commands List")
+		.setColor("RANDOM")
+		.setDescription(`${outputCommands()}`);
+	interaction.reply({ embeds: [embed] });
+};
 
 export default helpCommand;
