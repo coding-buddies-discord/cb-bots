f0261f732976598004de81e8218d71e04df004bb
earn <waream2@gmail.com>
2022-03-02 22:25:27 -0500

    chore: move command list to models directory

--- src/message_replies/commandsList.js
+++
@@ -1,8 +0,0 @@
-//Add new commands to this array.
-const commandsList = [
-    {command: `!ping`, description: `View the bot response time in milliseconds.`},
-    {command: `@user++`, description: `If a user has been helpful to you, mention their username and add ++ to give them a point.`},
-    {command: `!points`, description: `View the top 5 users who have attained the most points in the current channel.`}
-]
-
-export default commandsList;
