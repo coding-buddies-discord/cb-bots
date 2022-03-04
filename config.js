1523e4881e06b8621b9d0962dfe1dd8549508ca2
earn <waream2@gmail.com>
2022-03-04 16:08:53 -0500

    add config to heroku

---
+++ config.js
@@ -0,0 +1,7 @@
+export default {
+	token: "OTM5NjAyMTMzMDA1Nzc0ODU4.Yf7OrQ.Ulc7CaEC37O8EsFf03S63jiMsLc",
+	clientId: "939602133005774858",
+	guildId: "939629912623575090",
+	prefix: "!",
+	suffix: "++",
+};
