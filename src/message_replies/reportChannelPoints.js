6c2c962f83aafb4523514f89480bfcb0bb58f95b
Claudi0-V <claudiov.dejesus@protonmail.com>
2022-07-04 17:16:06 -0300

    adds imageGenerator and changes the point command

--- src/message_replies/reportChannelPoints.js
+++ src/message_replies/reportChannelPoints.js
@@ -1,27 +1,40 @@
 import { channelPoints } from "../../db.js";
 import { isUserValid } from "../utils/isUserValid.js";
+import { imgFromHtmlGenerator, generateBody } from "../image_templates/imgFromHtmlGenerator.js";
+import { leaderBoardBody, styles } from "../image_templates/points.js"
 
 const reportChannelPoints = async (interaction) => {
 	const { channelId } = interaction;
-	const topPointEarners = await channelPoints(channelId, 5);
+	const topPointEarners = await channelPoints(channelId, 8);
 
-	let currentChannelPoints = topPointEarners.length;
-
-
-	if (currentChannelPoints === 0) return interaction.reply("There are no points in this channel yet, you should give someone one.😏");
-
-	const validUsers = [];
-	for (const { userID, points } of topPointEarners) {
-		if (points === 0) continue;
-		const { username, validUser } = await isUserValid(interaction, userID);
-		if (validUser) validUsers.push({ username, points });
+	if (topPointEarners.length === 0) {
+		return interaction.reply(
+			"There are no points in this channel yet, you should give someone one.😏"
+		);
 	}
 
-	const message = validUsers.reduce((acc, { username, points }) => {
-		return acc + `${username}: ${points} Points\n`;
-	}, `<#${channelId}> **top ${validUsers.length}**:\n`);
+	const arrOfValidUsers = [];
+	for (const { userID, points } of topPointEarners) {
+		if (points === 0) continue;
+		const { username, validUser, user } = await isUserValid(
+			interaction,
+			userID
+		);
+		if (validUser) {
+			arrOfValidUsers.push({ username, points, user });
+		}
+	}
 
-	interaction.reply(message);
+	const caller = interaction.author.username;
+	const channelName = interaction.channel.name;
+	const userListHTML = leaderBoardBody(channelName, caller, arrOfValidUsers);
+	const body = generateBody(userListHTML, styles);
+
+	const img = await imgFromHtmlGenerator(body)
+	interaction.reply({
+
+	 	files: [{ attachment: img }],
+	 });
 };
 
 export default reportChannelPoints;
