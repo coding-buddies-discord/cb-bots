05cf3d9556e5056eecbdb20975f3577351a94cc3
Claudio V <claudiov.dejesus@protonmail.com>
2022-12-23 16:15:46 -0300

    refactors function to return an object.

--- src/utils/getUserIdFromMention.js
+++ src/utils/getUserIdFromMention.js
@@ -1,5 +1,11 @@
-export const getUserIdFromMention = (command) => {
-  const userId = command.match(/(?!0)+(\d)+/g);
+export const getUserIdFromMention = (IDstr) => {
+  const userId = IDstr.match(/(?!0)+(\d)+/g);
+  const id = userId?.[0];
 
-  return userId?.[0] || 0;
+  if (!id) {
+    const falseID = IDstr.match(/\d+/g)?.[0] || 0;
+    return { isValidID: false, id: falseID };
+  }
+
+  return { id, isValidID: true };
 };
