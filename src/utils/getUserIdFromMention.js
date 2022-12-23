0f21b2cf869f447d9406c62758f25b4e53ff0745
Claudio V <claudiov.dejesus@protonmail.com>
2022-12-23 13:52:52 -0300

    refactor: removes zeros from begin of ID string

--- src/utils/getUserIdFromMention.js
+++ src/utils/getUserIdFromMention.js
@@ -1,4 +1,5 @@
 export const getUserIdFromMention = (command) => {
-  const userId = command.match(/\d+/g);
-  return userId?.[0] || false;
+  const userId = command.match(/(?!0)+(\d)+/g);
+
+  return userId?.[0] || 0;
 };
