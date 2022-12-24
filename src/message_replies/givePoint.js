5eb2a910659151d477dc53baa728c6c00ff1b4b5
Claudio V <claudiov.dejesus@protonmail.com>
2022-12-24 11:08:07 -0300

    add discord verification

--- src/message_replies/givePoint.js
+++ src/message_replies/givePoint.js
@@ -4,68 +4,39 @@ import BuddiesModel from '../../models/BuddiesModel.js';
 
 async function givePoint(commandArr, interaction) {
   const caller = interaction.author.id;
-  const mentionIDs = commandArr.map((command) => getUserIdFromMention(command));
 
-  if (mentionIDs.length > 10) {
-    mentionIDs.splice(10, 1);
+  let mentionIDs = commandArr.map((command) => getUserIdFromMention(command));
+  mentionIDs = mentionIDs.reduce((acc, IDObj) => {
+    const filteredLength = acc.filter(({ id }) => id !== IDObj.id).length;
+    if (acc.length === filteredLength) {
+      acc.push(IDObj);
+      return acc;
+    }
+    return acc;
+  }, []);
+
+  if (mentionIDs.length > 9) {
+    mentionIDs = mentionIDs.slice(0, 9);
+
     await interaction.reply(
-      `Sorry <@!${caller}>, You can only give points to 10 people per message.
-Points will be given ONLY for the first 10 people mentioned`
-    );
-  }
-
-  if (!mentionIDs.length) {
-    return interaction.reply(
-      `Sorry <@!${caller}>, can't find ${
-        (BuddiesModel, isUserValid)
-      }.\n(╯°□°）╯︵ ┻━┻`
+      `Sorry <@!${caller}>, You can only give points to 9 people per message.
+Points will be given ONLY for the first 9 people mentioned`
     );
   }
 
   const validIDs = mentionIDs.filter((mentionID) => mentionID.isPossibleID);
-  const invalidIDs = mentionIDs.filter((mentionID) => !mentionID.isPossibleID);
 
-  for await (const id of validIDs) {
-    console.log('for loop -->', id);
-    console.log(invalidIDs);
+  const discordVerified = [];
+  for (const idObj of validIDs) {
+    const { validUser, username: name } = await isUserValid(
+      interaction,
+      idObj.id
+    );
+    if (validUser) {
+      discordVerified.push({ ...idObj, name });
+    }
   }
-  // const { validUser, username } = await isUserValid(interaction, mentionId);
-
-  // // <@123456789012345678> will be an id but not a valid one,
-  // // therefore will need to be checked, and will need this message;
-  // if (!validUser) {
-  //   interaction.reply(
-  //     // eslint-disable-next-line no-useless-escape
-  //     `Sorry <@!${caller}>, idk who ${command} is. ¯\\_(ツ)_/¯`
-  //   );
-  // } else if (caller === mentionId) {
-  //   interaction.reply(`Lmao <@!${caller}>, you can't give yourself a point.`);
-  // } else {
-  //   // try to add the user to the DB, if they are already there
-  //   // db function will reject this
-
-  //   await BuddiesModel.addUserToPoints(mentionId);
-
-  //   const canAddPoint = await BuddiesModel.testDates(mentionId, interaction);
-  //   if (!canAddPoint) {
-  //     interaction.reply(
-  //       `Yo **${interaction.author.username}**, you have to wait **at least** a minute to give **${username}** another point.😅`
-  //     );
-  //   }
-  //   if (canAddPoint) {
-  //     await BuddiesModel.giveUserAPoint(mentionId, interaction);
-
-  //     try {
-  //       const stonks = interaction.guild.emojis.cache.find(
-  //         (emoji) => emoji.name === 'stonks'
-  //       );
-  //       await interaction.react('🤖');
-  //       await interaction.react(stonks || '🔥');
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   }
-  // }
+  console.log(discordVerified, BuddiesModel);
 }
 
 export default givePoint;
